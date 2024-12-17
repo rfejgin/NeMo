770480d24a107f3afa5aa699ecbd6c2bae4b462e
Fejgin, Roy <rfejgin@nvidia.com>
2024-12-16 21:19:02 -0800

    Save context audios during evaluation

--- scripts/t5tts/infer_and_evaluate.py
+++ scripts/t5tts/infer_and_evaluate.py
@@ -11,11 +11,26 @@ import argparse
 import numpy as np
 import scipy.stats as stats
 
+"""
+Sample command line:
+ python scripts/t5tts/infer_and_evaluate.py --hparams_file /data/experiments/decoder_context/hparams.yaml --checkpoint_file /data/experiments/decoder_context/T5TTS--val_loss\=5.0848-epoch\=28.ckpt   --codecmodel_path /data/codec_checkpoints/codecs-no-eliz/AudioCodec_21Hz_no_eliz.nemo --datasets vctk --out_dir ./inference_output
+"""
+
 dataset_meta_info = {
     'vctk': {
-        'manifest_path' : '/home/pneekhara/2023/SimpleT5NeMo/manifests/smallvctk__phoneme__nemo_audio_21fps_8codebooks_2kcodes_v2bWithWavLM_simplet5_withcontextaudiopaths.json',
+        'manifest_path' : '/datap/misc/speechllm_codecdatasets/manifests/t5_exp/smallvctk__phoneme__nemo_audio_21fps_8codebooks_2kcodes_v2bWithWavLM_simplet5_withcontextaudiopaths.json',
         'audio_dir' : '/datap/misc/Datasets/VCTK-Corpus',
         'feature_dir' : '/datap/misc/Datasets/VCTK-Corpus',
+    },
+    'libri_dev_clean_eval_large': {
+        'manifest_path' : '/datap/misc/speechllm_codecdatasets/manifests/t5_exp/dev_clean_withContextAudioPaths_withTargetCodes_evalset_large.json',
+        'audio_dir' : '/datap/misc/Datasets/LibriTTS',
+        'feature_dir' : '/datap/misc/Datasets/LibriTTS',
+    },
+    'libri_dev_clean_eval_small': {
+        'manifest_path' : '/datap/misc/speechllm_codecdatasets/manifests/t5_exp/dev_clean_withContextAudioPaths_withTargetCodes_evalset.json',
+        'audio_dir' : '/datap/misc/Datasets/LibriTTS',
+        'feature_dir' : '/datap/misc/Datasets/LibriTTS',
     },
     'riva_challenging': {
         'manifest_path' : '/home/pneekhara/2023/SimpleT5NeMo/manifests/challengingLindyRodney__phoneme__nemo_audio_21fps_8codebooks_2kcodes_v2bWithWavLM_simplet5_withContextAudioPaths.json',
@@ -39,13 +54,22 @@ dataset_meta_info = {
     }
 }
 
+def write_audio_tensor(t: torch.Tensor, lengths: torch.Tensor, prefix:str, audio_dir:str, sample_rate:int, item_index:int):
+    for idx in range(t.size(0)):
+        audio_np = t[idx].float().detach().cpu().numpy()
+        audio_np = audio_np[:lengths[idx]]
+        audio_path = os.path.join(audio_dir, f"{prefix}_{item_index}.wav")
+        sf.write(audio_path, audio_np,sample_rate)
+        item_index += 1
+    return item_index
+        
+
 def compute_mean_and_confidence_interval(metrics_list, metric_keys, confidence=0.90):
     metrics = {}
     for key in metric_keys:
         measurements = [m[key] for m in metrics_list]
         mean = np.mean(measurements)
         std_err = stats.sem(measurements)
-
         confidence_interval = std_err * stats.t.ppf((1 + confidence) / 2, len(measurements) - 1)
         print(f"{key}: {mean} +/- {confidence_interval}")
         metrics[key] = "{:.4f} +/- {:.4f}".format(mean, confidence_interval)
@@ -138,13 +162,12 @@ def run_inference(hparams_file, checkpoint_file, datasets, out_dir, temperature,
                 predicted_audio, predicted_audio_lens, _, _ = model.infer_batch(batch_cuda, max_decoder_steps=500, temperature=temperature, topk=topk, use_cfg=use_cfg, cfg_scale=cfg_scale)
                 et = time.time()
                 print(f"Time taken for inference: {et-st}", predicted_audio.size())
-                for idx in range(predicted_audio.size(0)):
-                    predicted_audio_np = predicted_audio[idx].float().detach().cpu().numpy()
-                    predicted_audio_np = predicted_audio_np[:predicted_audio_lens[idx]]
-                    audio_path = os.path.join(audio_dir, f"predicted_audio_{item_idx}.wav")
-                    sf.write(audio_path, predicted_audio_np, model.cfg.sample_rate)
-                    item_idx += 1
-            
+                write_audio_tensor(t=predicted_audio, lengths=predicted_audio_lens, prefix="predicted_audio", 
+                                audio_dir=audio_dir, sample_rate=model.cfg.sample_rate, item_index=item_idx)
+                write_audio_tensor(t=batch['context_audio'], lengths=batch['context_audio_lens'], prefix="context_audio", 
+                                audio_dir=audio_dir, sample_rate=model.cfg.sample_rate, item_index=item_idx)
+                item_idx += predicted_audio.size(0)
+        
             metrics, filewise_metrics = evaluate_generated_audio.evaluate(
                 dataset_meta[dataset]['manifest_path'],
                 dataset_meta[dataset]['audio_dir'],
@@ -180,16 +203,16 @@ def run_inference(hparams_file, checkpoint_file, datasets, out_dir, temperature,
 
 def main():
     parser = argparse.ArgumentParser(description='Experiment Evaluation')
-    parser.add_argument('--hparams_files', type=str, default="/datap/misc/continuouscheckpoints_ks3ks3/multiencoder_small_sp_ks3_hparams.yaml,/datap/misc/continuouscheckpoints_ks3ks3/decodercontext_small_sp_ks3Correct_hparams.yaml")
-    parser.add_argument('--checkpoint_files', type=str, default="/datap/misc/continuouscheckpoints_ks3ks3/multiencoder_small_sp_ks3_epoch302.ckpt,/datap/misc/continuouscheckpoints_ks3ks3/decodercontext_small_sp_ks3Correct_epoch305.ckpt")
-    parser.add_argument('--codecmodel_path', type=str, default="/datap/misc/checkpoints/AudioCodec_21Hz_no_eliz.nemo")
-    parser.add_argument('--datasets', type=str, default="libri_unseen_val,riva_challenging_nozeros")
-    parser.add_argument('--base_exp_dir', type=str, default="/datap/misc/eosmount_4")
+    parser.add_argument('--hparams_file', type=str)
+    parser.add_argument('--checkpoint_file', type=str)
+    parser.add_argument('--codecmodel_path', type=str, default="/data/codec_checkpoints/codecs-no-eliz/AudioCodec_21Hz_no_eliz.nemo")
+    parser.add_argument('--datasets', type=str, default="libri_dev_clean_eval_large")
+    parser.add_argument('--base_exp_dir', type=str, default="/home/rfejgin/portfolio-cs-oci/experiments/")
     parser.add_argument('--draco_exp_dir', type=str, default="/lustre/fsw/llmservice_nemo_speechlm/users/pneekhara/gitrepos/experiments/NewT5TTS_FixedPosEmb/AllKernselSize3")
-    parser.add_argument('--server_address', type=str, default="pneekhara@login-eos02.eos.clusters.nvidia.com")
-    parser.add_argument('--exp_names', type=str, default="decodercontext_small_sp_ks3Correct_decdropout,decodercontext_small_sp_ks3Correct")
-    parser.add_argument('--local_ckpt_dir', type=str, default="/datap/misc/continuouscheckpoints_fixedposembrough")
-    parser.add_argument('--out_dir', type=str, default="/datap/misc/ContinuousEvalResults/KS3_KS3_ContinuousEval")
+    parser.add_argument('--server_address', type=str, default="rfejgin@cs-oci-ord-dc-03.nvidia.com")
+    parser.add_argument('--exp_names', type=str, default=None)
+    parser.add_argument('--local_ckpt_dir', type=str, default="/datap/misc/continuouscheckpoints")
+    parser.add_argument('--out_dir', type=str)
     parser.add_argument('--temperature', type=float, default=0.6)
     parser.add_argument('--use_cfg', action='store_true')
     parser.add_argument('--cfg_scale', type=float, default=1.0)

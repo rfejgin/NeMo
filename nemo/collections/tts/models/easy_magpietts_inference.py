vklimkov-nvidia/Speech

Easymagpie: introduce masking of acoustic tokens in backbone input and in refiner layers
Signed-off-by: Viacheslav Klimkov <vklimkov@nvidia.com>

nemo/collections/tts/models/easy_magpietts_inference.py
@@ -451,6 +451,12 @@ class EasyMagpieTTSInferenceModel(ModelPT):
             else:
                 self.audio_in_projection = nn.Identity()
 
+        # Train-time masking of the audio history the decoder reads. Hidden frames go in as the
+        # mask token, which is the model's own marker for a position whose codes are not known.
+        self.mask_audio_history = cfg.get('mask_audio_history', False)
+        self.audio_history_mask_min = cfg.get('audio_history_mask_min', 0.0)
+        self.audio_history_mask_max = cfg.get('audio_history_mask_max', 0.75)
+
         # Speaker/context encoder for context audio embeddings.
         # This enables keeping the zero-shot conditioning module private at release time.
 
@@ -698,6 +704,9 @@ class EasyMagpieTTSInferenceModel(ModelPT):
                     cfg.get('acoustic_refiner_prediction_schedule', AcousticRefiner.PREDICTION_SCHEDULE)
                 ),
                 commit_order=cfg.get('acoustic_refiner_commit_order', AcousticRefinerOrder.CODEBOOK),
+                mask_codes=cfg.get('acoustic_refiner_mask_codes', False),
+                mask_min=cfg.get('acoustic_refiner_mask_min', 0.0),
+                mask_max=cfg.get('acoustic_refiner_mask_max', 0.9),
             )
 
     @property

rfejgin/NeMo

nemo/collections/tts/models/easy_magpietts_inference.py
@@ -437,6 +437,12 @@ class EasyMagpieTTSInferenceModel(ModelPT):
         else:
             self.audio_in_projection = nn.Identity()
 
+        # Train-time masking of the audio history the decoder reads. Hidden frames go in as the
+        # mask token, which is the model's own marker for a position whose codes are not known.
+        self.mask_audio_history = cfg.get('mask_audio_history', False)
+        self.audio_history_mask_min = cfg.get('audio_history_mask_min', 0.0)
+        self.audio_history_mask_max = cfg.get('audio_history_mask_max', 0.75)
+
         # Speaker/context encoder for context audio embeddings.
         # This enables keeping the zero-shot conditioning module private at release time.
 
@@ -683,6 +689,9 @@ class EasyMagpieTTSInferenceModel(ModelPT):
                 codebook_size=self.codebook_size,
                 prediction_schedule=prediction_schedule,
                 n_layers=cfg.get('acoustic_codes_predictor_n_layers', 1),
+                mask_codes=cfg.get('acoustic_codes_predictor_mask_codes', False),
+                mask_min=cfg.get('acoustic_codes_predictor_mask_min', 0.0),
+                mask_max=cfg.get('acoustic_codes_predictor_mask_max', 0.9),
             )
 
     @property